e214a55e9d0ac715cabd748ef83bfc73e7e20dc3
ACER <ACER@DESKTOP-M57CAPD>
2023-01-19 17:19:52 +0530

    update

---
+++ src/main/java/Practice_Maven/CreateTest.java
@@ -0,0 +1,25 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	
+	@Test
+	public void create()
+	{
+	String BROWSER = System.getProperty("browser");
+	String URL = System.getProperty("url");
+	String USERNAME = System.getProperty("username");
+	String PASSWORD = System.getProperty("password");
+	
+	
+	System.out.println(BROWSER);
+	System.out.println(URL);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
+	
+	
+	
+}
+}
---
+++ src/main/java/Practice_Maven/UpdateTest.java
@@ -0,0 +1,19 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class UpdateTest {
+	
+	@Test
+	public void update()
+	{
+		System.out.println("-- TestScript- 3");
+	}
+	
+	@Test
+	public void delete()
+	{
+		System.out.println("-- TestScript deleted --");
+	}
+
+}
